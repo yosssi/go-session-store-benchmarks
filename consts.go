91e3dcdec08aa8c5607c834a05b9c0b4e92707b8
yosssi <yoshida.keiji.84@gmail.com>
2014-06-19 01:51:36 +0900

    Update get_test.go

--- consts.go
+++ consts.go
@@ -1,10 +1,18 @@
 package main
 
+// constants for common usage
 const (
 	secretKey  = "secret-key"
 	sessionKey = "session-key"
 )
 
+// constants for yosssi/boltstore
 const (
 	boltDBPath = "sessions.db"
+)
+
+// constants for bradleypeabody/gorilla-sessions-memcache
+const (
+	memcacheServer = "localhost:11211"
+	sessionPrefix  = "session_prefix_"
 )
--- get_test.go
+++ get_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/boltdb/bolt"
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/bradleypeabody/gorilla-sessions-memcache"
 	"github.com/yosssi/boltstore/store"
 )
 
@@ -29,9 +31,40 @@ func BenchmarkBoltstore_Get(b *testing.B) {
 		b.Error(err)
 	}
 
+	// Reset the timer.
+	b.ResetTimer()
+
 	// Get a session.
-	_, err = str.Get(req, sessionKey)
+	for i := 0; i < b.N; i++ {
+		_, err = str.Get(req, sessionKey)
+		if err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkGorillaSessionsMemcache(b *testing.B) {
+	// Create a client.
+	client := memcache.New(memcacheServer)
+
+	// Create a store.
+	str := gsm.NewMemcacheStore(client, sessionPrefix, []byte(secretKey))
+
+	// Create a request.
+	req, err := http.NewRequest("GET", "http://localhost:3000/", nil)
 	if err != nil {
 		b.Error(err)
 	}
+
+	// Reset the timer.
+	b.ResetTimer()
+
+	// Get a session.
+	for i := 0; i < b.N; i++ {
+		_, err = str.Get(req, sessionKey)
+		if err != nil {
+			b.Error(err)
+		}
+	}
+
 }
