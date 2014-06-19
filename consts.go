2aa9abe63deebbb0564ae064b2b7626280d1741f
yosssi <yoshida.keiji.84@gmail.com>
2014-06-19 15:19:33 +0900

    Update get_test.go

--- consts.go
+++ consts.go
@@ -2,8 +2,14 @@ package main
 
 // constants for common usage
 const (
-	secretKey  = "secret-key"
-	sessionKey = "session-key"
+	secretKey     = "secret-key"
+	sessionKey    = "session-key"
+	sessionPrefix = "session_prefix_"
+)
+
+// constants for gorilla/sessions.FilesystemStore
+const (
+	filesystemStorePath = "sessions.txt"
 )
 
 // constants for yosssi/boltstore
@@ -14,5 +20,33 @@ const (
 // constants for bradleypeabody/gorilla-sessions-memcache
 const (
 	memcacheServer = "localhost:11211"
-	sessionPrefix  = "session_prefix_"
+)
+
+// constants for kidstuff/mongostore
+const (
+	mongoServer     = "localhost"
+	mongoDB         = "test"
+	mongoCollection = "test_session"
+)
+
+// constants for srinathgs/mysqlstore
+const (
+	mySQLServer    = "@tcp(localhost:3306)/sessions?parseTime=true&loc=Local"
+	mySQLTableName = "sessions"
+)
+
+// constants for antonlindstrom/pgstore
+const (
+	postgreSQLServer = "postgres://%s:%s@127.0.0.1:5432/sessions?sslmode=disable"
+)
+
+// constants for boj/redistore
+const (
+	redisServer = ":6379"
+)
+
+// constants for boj/riakstore
+const (
+	riakServer = "127.0.0.1:8098"
+	riakBucket = "sessions"
 )
--- get_test.go
+++ get_test.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"testing"
+	"labix.org/v2/mgo"
 
+	"github.com/antonlindstrom/pgstore"
+	"github.com/boj/redistore"
+	"github.com/boj/riakstore"
 	"github.com/boltdb/bolt"
 	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/bradleypeabody/gorilla-sessions-memcache"
+	"github.com/gorilla/sessions"
+	"github.com/kidstuff/mongostore"
+	"github.com/srinathgs/mysqlstore"
 	"github.com/yosssi/boltstore/store"
 )
+
+func BenchmarkCookieStore_Get(b *testing.B) {
+	// Create a store.
+	str := sessions.NewCookieStore([]byte(secretKey))
+
+	// Benchmark.
+	benchmark(b, str)
+}
+
+func BenchmarkFilesystemStore_Get(b *testing.B) {
+	// Create a store.
+	str := sessions.NewFilesystemStore(filesystemStorePath, []byte(secretKey))
+
+	// Benchmark.
+	benchmark(b, str)
+}
 
 func BenchmarkBoltstore_Get(b *testing.B) {
 	// Open a Bolt database.
@@ -25,22 +50,8 @@ func BenchmarkBoltstore_Get(b *testing.B) {
 		b.Error(err)
 	}
 
-	// Create a request.
-	req, err := http.NewRequest("GET", "http://localhost:3000/", nil)
-	if err != nil {
-		b.Error(err)
-	}
-
-	// Reset the timer.
-	b.ResetTimer()
-
-	// Get a session.
-	for i := 0; i < b.N; i++ {
-		_, err = str.Get(req, sessionKey)
-		if err != nil {
-			b.Error(err)
-		}
-	}
+	// Benchmark.
+	benchmark(b, str)
 }
 
 func BenchmarkGorillaSessionsMemcache(b *testing.B) {
@@ -50,6 +61,73 @@ func BenchmarkGorillaSessionsMemcache(b *testing.B) {
 	// Create a store.
 	str := gsm.NewMemcacheStore(client, sessionPrefix, []byte(secretKey))
 
+	// Benchmark.
+	benchmark(b, str)
+}
+
+func BenchmarkMongostore(b *testing.B) {
+	// Create a database session.
+	dbsess, err := mgo.Dial(mongoServer)
+	if err != nil {
+		panic(err)
+	}
+
+	defer dbsess.Close()
+
+	// Create a store.
+	str := mongostore.NewMongoStore(dbsess.DB(mongoDB).C(mongoCollection), 3600, true, []byte(secretKey))
+
+	// Benchmark.
+	benchmark(b, str)
+}
+
+func BenchmarkMysqlstore(b *testing.B) {
+	// Create a store.
+	str, err := mysqlstore.NewMySQLStore(os.Getenv("MYSQL_USER")+":"+os.Getenv("MYSQL_PASSWORD")+mySQLServer, mySQLTableName, "/", 3600, []byte(secretKey))
+	if err != nil {
+		panic(err)
+	}
+
+	defer str.Close()
+
+	// Benchmark.
+	benchmark(b, str)
+}
+
+func BenchmarkPgstore(b *testing.B) {
+	// Create a store.
+	str := pgstore.NewPGStore(fmt.Sprintf(postgreSQLServer, os.Getenv("PG_USER"), os.Getenv("PG_PASSWORD")), []byte(secretKey))
+
+	defer str.Close()
+
+	// Benchmark.
+	benchmark(b, str)
+}
+
+func BenchmarkRedistore(b *testing.B) {
+	// Create a store.
+	str, err := redistore.NewRediStore(10, "tcp", redisServer, "", []byte(secretKey))
+	if err != nil {
+		panic(err)
+	}
+
+	defer str.Close()
+
+	// Benchmark.
+	benchmark(b, str)
+}
+
+func BenchmarkRiakstore(b *testing.B) {
+	// Create a store.
+	str := riakstore.NewRiakStore([]string{riakServer}, 5, riakBucket, []byte(secretKey))
+
+	defer str.Close()
+
+	// Benchmark.
+	benchmark(b, str)
+}
+
+func benchmark(b *testing.B, str sessions.Store) {
 	// Create a request.
 	req, err := http.NewRequest("GET", "http://localhost:3000/", nil)
 	if err != nil {
@@ -67,4 +145,6 @@ func BenchmarkGorillaSessionsMemcache(b *testing.B) {
 		}
 	}
 
+	// Stop the timer.
+	b.StopTimer()
 }
